8c433873f328f7e087f52ddaf2396709e9cb6341
Deb Koch <92552721+DebraMK@users.noreply.github.com>
2022-07-31 13:09:22 -0500

    updated homepage styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,31 +7,34 @@ import Card from 'react-bootstrap/Card';
 
 export default function Home () {
     return(
-        <Container fluid>
+        <Container style={{ }}>
                 <header>
                     <h1>CDEV Quizzes</h1>
                     <h2>Test your knowledge!</h2>
-                    <p>Choose a category and a quiz to text how much you know.  Or, create your own quiz to challenge others!</p>
+                    <p>Choose a category and a quiz to test how much you know.  Or, create your own quiz to challenge others!</p>
                 </header>  
             <Row>  
                 <Col> 
-                    <Card style={{ backgroundColor: 'red' }}>
+                    <Card style={{ height: 220, backgroundColor: 'red', alignItems: 'center' }}>
                         <Nav>
                             <Nav.Link style={{ color: 'black', fontFamily: 'fantasy', fontSize: '30px' }} href="/list">Take A Quiz</Nav.Link>
                         </Nav>
+                        <Card.Img variant="bottom" src="https://cdn.pixabay.com/photo/2020/09/23/07/53/quiz-5595288__340.jpg" />
                     </Card> 
+                    
                 </Col>
                 <Col>
-                    <Card className="lg-dark text-white">
+                    <Card style={{ height: 220, backgroundColor: 'palegreen', fontFamily: 'fantasy', fontSize: '20px' }}>
                         <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/03/02/09/26/question-mark-2110767_1280.jpg" alt="Card image" />
                         <Card.Text>So many questions!</Card.Text>
                     </Card>
                 </Col>
                 <Col>
-                    <Card style={{ backgroundColor: 'yellow' }}>
+                    <Card style={{ height: 220, backgroundColor: 'yellow', alignItems: 'center' }}>
                         <Nav>
                             <Nav.Link style={{ color: 'black', fontFamily: 'fantasy', fontSize: '30px' }} href="/create">Create A Quiz</Nav.Link>
                         </Nav>
+                        <Card.Img variant="bottom" src="https://cdn.pixabay.com/photo/2021/03/16/11/31/pencils-6099511_960_720.jpg" />
                     </Card>
                 </Col>
             </Row> 
