c842b754f5d9295fe1d61339cce329cf6e7af3c2
Conley Brown <conleybrown@Conleys-MacBook-Pro.local>
2022-08-02 19:32:28 -0400

    changes to create and update functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import NewQuizForm from './Components/NewQuizForm2';
 import List from './Components/List';
 import Quiz from './Components/Quiz';
+import UpdateQuiz from './Components/UpdateQuiz';
 
 import './CSS/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,7 +19,7 @@ function App() {
         <Route path='/create' element={<NewQuizForm />}/>
         <Route path='/list' element={<List />}/>
         <Route path='/quiz/:id' element={<Quiz />}/>
-
+        <Route path='/quiz/update/:id' element={<UpdateQuiz />}/>
         </Routes>
       </Router>
     </div>
